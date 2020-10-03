499b1dc4611c8819c83dce910d5d1de3dd4acca7
Cyberboa <72196905+Cyberboa@users.noreply.github.com>
2020-10-03 20:17:49 +0200

    Exercise 1 + 2

--- testside/pages/tests.py
+++ testside/pages/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+# Exercise 1
+words = "hello world"
+listOfCharacters = list
+
+"""index starts with 0 / words -2 the right ending"""
+for index in range(0, len(words) - 2):
+        """The 3 Characters will be written in the variable listOfCharacters"""
+        listOfCharacters = words[index:index + 3]
+        print(listOfCharacters)
