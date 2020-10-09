3794e9446d82ae5ff3b3e4cdf487670e446b2467
Cyberboa <72196905+Cyberboa@users.noreply.github.com>
2020-10-09 09:59:24 +0200

    Exercise 6

--- testside/pages/Exercise 6.py
+++ testside/pages/Exercise 6.py
@@ -6,29 +6,28 @@ from django.test import TestCase
 def cut_string(string):
     listOfCharacters = []
     string = input('Type in a word')
+    file = open('myScript.txt', "w")
     """index starts with 0 / words -2 the right ending"""
     for index in range(0, len(string) - 2):
         "Insert String to list"
         listOfCharacters.insert(index, string[index:index + 3])
         "Insert String to myScript.txt"
-        add_to_file(string[index:index + 3])
+        add_to_file(string[index:index + 3], file)
     """Sort List"""
     listOfCharacters.sort()
+    # close the file
+    file.close()
     return listOfCharacters
 
 
 """Method to add string to file"""
 
 
-def add_to_file(string):
-    # open file with
-    file = open('myScript.txt', "a")
+def add_to_file(string, file):
     # write the string into thefile
     file.write("".join(string))
     # creating a new line
     file.write("\n")
-    # close the file
-    file.close()
     return file
 
 
