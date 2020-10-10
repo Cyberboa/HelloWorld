2bdf90e70f67a6267ef043bf4b328c4d0cc431d7
Cyberboa <72196905+Cyberboa@users.noreply.github.com>
2020-10-10 14:16:06 +0200

    - Exercise setter Methode

--- testside/pages/dice.py
+++ testside/pages/dice.py
@@ -29,6 +29,24 @@ class Dice:
     def get_side_six(self):
         return self.side_six
 
+    def set_side_one(self, new_side_one):
+        self.side_one = new_side_one
+
+    def set_side_two(self, new_side_two):
+        self.side_one = new_side_two
+
+    def set_side_three(self, new_side_three):
+        self.side_one = new_side_three
+
+    def set_side_four(self, new_side_four):
+        self.side_one = new_side_four
+
+    def set_side_five(self, new_side_five):
+        self.side_one = new_side_five
+
+    def set_side_six(self, new_side_six):
+        self.side_one = new_side_six
+
     def throw(self):
         random_number = [self.get_side_one(), self.get_side_two(), self.get_side_three(), self.get_side_four(),
                          self.get_side_five(), self.get_side_six()]
